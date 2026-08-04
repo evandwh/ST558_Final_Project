68aae90ed0cf6f23b53b2a5fc3a1890a4b8eb690
Evan Whitfield <Evan>
2026-08-04 09:29:22 -0400

    Added docker file

--- myAPI.R
+++ myAPI.R
@@ -1,30 +1,29 @@
 library(plumber)
 library(tidymodels)
 library(ggplot2)
+library(dplyr)
+library(yardstick)
 
 # Read in data
 
 water_data <- read_csv("Data/water_potability.csv") |>
   mutate(Potability = factor(Potability,
                              levels = c(0, 1),
-                             labels = c("Not Potable", "Potable")))
+                             labels = c("Not Potable", "Potable"))) |>
+  drop_na()
 
 # Fit best model on entire data
-#--------------------------------------------------
 
 rf_recipe <-
-  recipe(Potability ~ ., data = water_data) |>
-  step_impute_median(all_numeric_predictors()) |>
-  step_normalize(all_numeric_predictors())
+  recipe(Potability ~ ., data = water_data)
 
 rf_spec <-
   rand_forest(
     trees = 500,
-    mtry = 3,
-    min_n = 5
+    mtry = 8,
+    mode = "classification"
   ) |>
-  set_engine("ranger") |>
-  set_mode("classification")
+  set_engine("ranger")
 
 rf_workflow <-
   workflow() |>
@@ -37,15 +36,15 @@ rf_fit <- fit(rf_workflow, data = water_data)
 # Means for defaults
 #--------------------------------------------------
 
-mean_ph <- mean(water_data$ph, na.rm = TRUE)
-mean_hardness <- mean(water_data$Hardness, na.rm = TRUE)
-mean_solids <- mean(water_data$Solids, na.rm = TRUE)
-mean_chloramines <- mean(water_data$Chloramines, na.rm = TRUE)
-mean_sulfate <- mean(water_data$Sulfate, na.rm = TRUE)
-mean_conductivity <- mean(water_data$Conductivity, na.rm = TRUE)
-mean_organic <- mean(water_data$Organic_carbon, na.rm = TRUE)
-mean_trihalo <- mean(water_data$Trihalomethanes, na.rm = TRUE)
-mean_turbidity <- mean(water_data$Turbidity, na.rm = TRUE)
+mean_ph <- mean(water_data$ph)
+mean_hardness <- mean(water_data$Hardness)
+mean_solids <- mean(water_data$Solids)
+mean_chloramines <- mean(water_data$Chloramines)
+mean_sulfate <- mean(water_data$Sulfate)
+mean_conductivity <- mean(water_data$Conductivity)
+mean_organic <- mean(water_data$Organic_carbon)
+mean_trihalo <- mean(water_data$Trihalomethanes)
+mean_turbidity <- mean(water_data$Turbidity)
 
 #--------------------------------------------------
 #* Predict Potability
@@ -87,10 +86,16 @@ function(
   predict(rf_fit, new_data, type = "prob")
 }
 
-# Example calls:
-# http://localhost:8000/pred
-# http://localhost:8000/pred?ph=7
-# http://localhost:8000/pred?ph=8&Hardness=150&Solids=20000
+# -------------------------------------------------
+# Example URLs
+#
+# http://127.0.0.1:48521/pred?ph=7.086&Hardness=195.9681&Solids=21917.4414&Chloramines=7.1343&Sulfate=333.2247&Conductivity=426.5264&Organic_carbon=14.3577&Trihalomethanes=66.4009&Turbidity=3.9697
+#
+# http://127.0.0.1:48521/pred?ph=4&Hardness=195.9681&Solids=24000&Chloramines=7.1343&Sulfate=333.2247&Conductivity=426.5264&Organic_carbon=14.3577&Trihalomethanes=66.4009&Turbidity=3.9697
+#
+# http://127.0.0.1:48521/pred?ph=12&Hardness=200&Solids=21917.4414&Chloramines=7.1343&Sulfate=333.2247&Conductivity=426.5264&Organic_carbon=17&Trihalomethanes=68&Turbidity=4
+
+
 
 #--------------------------------------------------
 #* Information
@@ -107,18 +112,23 @@ function(){
 
 #--------------------------------------------------
 #* Confusion Matrix Plot
-#* @png
+#* @serializer png
 #* @get /confusion
 
 function(){
   
-  preds <-
-    predict(rf_fit, water_data) |>
+  preds <- predict(rf_fit, water_data) |>
     bind_cols(water_data)
   
-  cm <- conf_mat(preds,
-                 truth = Potability,
-                 estimate = .pred_class)
+  cm <- conf_mat(
+    preds, 
+    truth = Potability, 
+    estimate = .pred_class)
   
-  autoplot(cm, type = "heatmap")
-}
+  p <- ggplot(as.data.frame(cm$table), 
+              aes(x = Truth, y = Prediction, fill = Freq)) +
+    geom_tile() +
+    geom_text(ggplot2::aes(label = Freq), color = "white")
+  
+  print(p)
+  }
